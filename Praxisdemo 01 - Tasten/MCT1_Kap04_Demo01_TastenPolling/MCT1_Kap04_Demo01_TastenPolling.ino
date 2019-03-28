9518697785aaf9f467c1a9637aaba58e81868dc4
Prof. Just <olaf.just@w-hs.de>
2019-03-28 16:06:22 +0100

    Version für das SS2019

---
+++ Praxisdemo 01 - Tasten/MCT1_Kap04_Demo01_TastenPolling/MCT1_Kap04_Demo01_TastenPolling.ino
@@ -0,0 +1,33 @@
+// MCT1
+// MCT1_Kap04_Demo01_TastenPolling.ino
+// Version vom 28.3.2019
+//-------------------------------------
+// LED - Board an J5 (D16 -D23)
+// Tast - Board an J3 (D8-D15)
+//-------------------------------------
+const int LED_PIN = 23; // LED LA7 an D23
+const int BTN_PIN = 20;   // pushbutton SB7 an D8
+
+//--- Zustandsspeicher (kein bool da   int digitalRead() )
+int btnState = HIGH;
+int ledState = HIGH;
+
+void setup()
+{
+  pinMode(LED_PIN, OUTPUT);      // sets the digital pin 13 as output
+  pinMode(BTN_PIN, INPUT);      // sets the digital pin 7 as input
+}
+
+void loop()
+{
+  // --- Eingabe erfassen ---
+  btnState = digitalRead(BTN_PIN);   // read the input pin
+  
+  // --- Verarbeitung ---
+  // Eingabe = Ausgabe
+  ledState = btnState; 
+
+  // --- Ausgabe ---
+  digitalWrite(LED_PIN, ledState);
+  delay(2); //zeitliche Entprellung
+}
---
+++ Praxisdemo 01 - Tasten/MCT1_Kap04_Demo01b_TastenPolling/MCT1_Kap04_Demo01b_TastenPolling.ino
@@ -0,0 +1,46 @@
+// MCT1
+// MCT1_Kap04_Demo01b_TastenPolling.ino
+// Version vom 28.3.2019
+//-------------------------------------
+// LED - Board an J5 (D16 -D23)
+// Tast - Board an J3 (D8-D15)
+//-------------------------------------
+const int LED_PIN = 23; // LED LA7 an D23
+const int BTN_PIN = 20;   // pushbutton SB7 an D8
+
+//--- Zustandsspeicher (kein bool da   int digitalRead() )
+int btnState = HIGH;
+int btnStateOld = 0;
+int ledState = HIGH;
+
+//--------------------------------------
+void setup() {
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(BTN_PIN, INPUT);
+}
+//--------------------------------------
+void loop() {
+ // --- Eingabe erfassen ---
+  btnState = digitalRead(BTN_PIN);   // Taste pollen
+
+ // --- Verarbeitung ---
+  // -- Nur bei steigender Flanke wird Zustand der LED geaendert
+  //    LOW   Flanke  HIGH
+  //           |------------
+  //    -------|
+
+  // Rising Edge ?
+  if ( btnStateOld == LOW && btnState == HIGH) {
+    // Toogeln des LED-Zustands
+    if (ledState == HIGH) 
+      ledState = LOW;
+    else
+      ledState = HIGH;
+  }
+  //alten Zustand setzen
+  btnStateOld = btnState; 
+  
+ // --- Ausgabe ---
+  digitalWrite(LED_PIN, ledState);
+  delay(2); //zeitliche Entprellung
+}
