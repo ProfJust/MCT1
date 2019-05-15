2466ebeefac20b4f9d32b3edbb0ed1fa016a2de8
Prof. Just <olaf.just@w-hs.de>
2019-05-15 16:27:42 +0200

    Add files via upload

---
+++ Praxisdemo Kap11_PWM/V5_sw06_PWM_L_fter/V5_sw06_PWM_L_fter.ino
@@ -0,0 +1,39 @@
+//===============================================
+// ELS III - SS16
+// V5_sw06_PWM_Lüfter
+//---------------------------------------------
+// LED/POWER-Board an D0-D7
+// Zeigt LED-Dimmen durch PWM
+//---------------------------------------------
+
+//-------- Angeschlossen Hardware --------
+ #define  PWM_PIN 8 //PWM_PIN an D8
+ // On the Arduino Mega, PWM works on pins 2 - 13 and 44 - 46.
+ 
+//---- Variablen  ----
+   unsigned int  fade=50; // Duty-Cycle 0..255
+   
+//---- Initialisierung -----------------------
+void setup(){
+  //--- COM init ---
+  Serial.begin(9600);
+  //--- Ausgangspins -------
+   pinMode(PWM_PIN,OUTPUT); //Nicht notwendig
+}
+
+void loop(){
+  // -- neuen Duty-Cycle setzen --
+   fade++;
+   if(fade >=255) {
+    fade = 50;   
+    analogWrite(PWM_PIN, fade);   
+    delay(3000); 
+   }
+ 
+  //-- LED - Ausgabe --
+    analogWrite(PWM_PIN, fade);     
+  // -- LCD Ausgabe ---
+  Serial.println(fade); 
+  delay(200);   
+}
+
---
+++ Praxisdemo Kap11_PWM/V5_sw08_PWM_TimerOne_BSP/V5_sw08_PWM_TimerOne_BSP.ino
@@ -0,0 +1,32 @@
+//==========================================
+// Westfälische Hochschule - FB Maschinenbau
+// Prof. Dr.-Ing. Olaf Just
+// ELS III - SS16
+//--------------------------------------
+// V5_sw08_PWM_TimerOne_BSP.ino
+//-----E-Blocks ------------------------
+// Oszilloskop an D11 /D12
+
+#include <TimerOne.h>
+#define PinPWM 12   /* Timer 1 => Pin 11 oder 12 möglich*/
+#define PWM_PERIOD 50 /* 50usec = 20 kHz */
+int duty = 127;
+
+void setup(){
+  Serial.begin(9600);
+  pinMode(PinPWM, OUTPUT);
+  Timer1.initialize(1);            // Initialisierung auch für PWM, Wert ist dabei egal
+  Timer1.pwm(PinPWM, duty, PWM_PERIOD);   // setup pwm on pin 9, 12,5% duty cycle 50usec = 20 kHz
+}
+
+void loop(){
+  // Verändern des Tastverhätlnisses
+ Timer1.setPwmDuty(PinPWM, 1000);
+ delay(2000);
+  Timer1.setPwmDuty(PinPWM, 1500);
+   delay(2000);
+// if(duty>=1023) 
+Timer1.disablePwm(PinPWM); //Output auf LOW
+ Serial.println(duty);
+ delay(10);
+}
